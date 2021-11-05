1a1ff3c310bcdf0e6d3d015f5e26845d6c31c298
Christoph Haefner <christoph.haefner@mailbox.org>
2021-11-05 20:33:38 +0100

    Upgrade Board with cells HashMap
    Co-authored-by: Philipp Stadelmann <stadelmannphi@googlemail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
+use std::collections::HashMap;
+
 pub struct Board {
-    value: bool,
+    cells: HashMap<Coordinate, bool>,
 }
 
 impl Board {
     pub fn new() -> Self {
-        Self { value: false }
+        Self { cells: HashMap::new() }
     }
 
-    pub fn has_value(&self, _coordinate: Coordinate) -> bool {
-        self.value
+    pub fn has_value(&self, coordinate: Coordinate) -> bool {
+        *self.cells.get(&coordinate).unwrap_or(&false)
     }
 
-    pub fn set_value(&mut self, _coordinate: Coordinate, _value: u8) {
-        self.value = true;
+    pub fn set_value(&mut self, coordinate: Coordinate, _value: u8) {
+        self.cells.insert(coordinate, true);
     }
 }
 
@@ -22,6 +24,7 @@ impl Default for Board {
     }
 }
 
+#[derive(Hash, Eq, PartialEq)]
 pub struct Coordinate {
     pub row: u8,
     pub column: u8,
@@ -33,10 +36,13 @@ mod tests {
     use crate::Board;
     use crate::Coordinate;
 
+    const FIRST_COORDINATE: Coordinate = Coordinate { row: 0, column: 0 };
+    const SECOND_COORDINATE: Coordinate = Coordinate { row: 1, column: 0 };
+
     #[test]
     fn it_initializes_an_empty_baord() {
         let b = Board::default();
-        assert_eq!(b.has_value(Coordinate { row: 0, column: 0 }), false);
+        assert_eq!(b.has_value(FIRST_COORDINATE), false);
     }
 
     #[test]
@@ -45,9 +51,33 @@ mod tests {
         let mut b = Board::new();
 
         // Act
-        b.set_value(Coordinate { row: 0, column: 0 }, 2);
+        b.set_value(FIRST_COORDINATE, 2);
 
         // Assert
-        assert_eq!(b.has_value(Coordinate { row: 0, column: 0 }), true);
+        assert_eq!(b.has_value(FIRST_COORDINATE), true);
     }
+
+    #[test]
+    fn it_holds_two_values() {
+        let mut b = Board::default();
+        b.set_value(FIRST_COORDINATE, 2);
+        b.set_value(SECOND_COORDINATE, 2);
+
+        assert_eq!(b.has_value(FIRST_COORDINATE), true);
+        assert_eq!(b.has_value(SECOND_COORDINATE), true);
+    }
+
+    #[test]
+    fn it_sets_a_specific_cell() {
+        // Arrange
+        let mut b = Board::new();
+
+        // Act
+        b.set_value(FIRST_COORDINATE, 2);
+
+        // Assert
+        assert_eq!(b.has_value(SECOND_COORDINATE), false);
+    }
+
+
 }
