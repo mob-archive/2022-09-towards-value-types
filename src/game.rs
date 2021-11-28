0a5c11d3790e75806d5e4c0b2108cc6a62faf62f
Christoph Haefner <christoph.haefner@mailbox.org>
2021-11-28 20:23:08 +0100

    Avoid second hardcoded loop in Game.get_field
    Co-authored-by: Philipp Stadelmann <stadelmannphi@googlemail.com>

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,7 @@
-use crate::game_actions::move_down;
 use crate::board::Board;
 use crate::board_value::BoardValue;
 use crate::coordinate::Coordinate;
+use crate::game_actions::move_down;
 
 pub struct Game {
     board: Board,
@@ -13,9 +13,7 @@ impl Game {
     pub fn new() -> Self {
         let mut board = Board::default();
         board.set_value(Coordinate { row: 2, column: 0 }, BoardValue::new(2));
-        Self {
-            board,
-        }
+        Self { board }
     }
 
     pub fn get_score(&self) -> u128 {
@@ -23,16 +21,21 @@ impl Game {
     }
 
     pub fn get_field(&self) -> ExternalFieldRepresentation {
-        let field = self.board.get_representation();
-        let mut result = [[0u32; 4]; 4];
-
-        for row in 0..4 {
-            for column in 0..4 {
-                result[row as usize][column as usize] =
-                    field[row as usize][column as usize].get_value();
-            }
-        }
-        result
+        let field_iter = self.board.get_representation().into_iter();
+        // Rust Vector<->Array madness solved with help from
+        // https://stackoverflow.com/a/63355120 (and comments)
+        field_iter
+            .map(|column| {
+                column
+                    .into_iter()
+                    .map(|cell| cell.get_value())
+                    .collect::<Vec<u32>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<[u32; 4]>>()
+            .try_into()
+            .unwrap()
     }
 
     pub fn move_down(&mut self) {
