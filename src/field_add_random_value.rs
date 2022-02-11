e3c304b1b7ba2b8c428d0171baa852c3d1464d3c
Christoph Haefner <christoph.haefner@mailbox.org>
2022-02-11 19:20:13 +0100

    Finish field_add_random_value
    Co-authored-by: Philipp Stadelmann <stadelmannphi@googlemail.com>

--- src/field_add_random_value.rs
+++ src/field_add_random_value.rs
@@ -6,14 +6,28 @@ use crate::random::RandomNumber;
 pub fn add_value(
     field: Field,
     random_number_value: RandomNumber,
-    _random_number_position: RandomNumber,
+    random_number_position: RandomNumber,
 ) -> Field {
-    let new_number: BoardValue = get_new_number(random_number_value);
-    let vec: Vec<Coordinate> = get_coordinates_of_empty_cells(field);
-    let mut new_field: Field = field;
-    let first_coordinate: Coordinate = vec[0];
-    new_field[first_coordinate.row as usize][first_coordinate.column as usize] = new_number;
+    let mut new_field = field;
+    let new_board_value = get_new_board_value(random_number_value);
+    let coordinate = get_coordinate(field, random_number_position);
+    new_field[coordinate.row as usize][coordinate.column as usize] = new_board_value;
     new_field
+}
+
+fn get_coordinate(field: Field, random_number: RandomNumber) -> Coordinate {
+    let vec: Vec<Coordinate> = get_coordinates_of_empty_cells(field);
+    let coordinate_index = get_index_for_coordinate(random_number, vec.len());
+    vec[coordinate_index]
+}
+
+fn get_index_for_coordinate(random_number: RandomNumber, free_coordinate_count: usize) -> usize {
+    // There is no 100% even distribution!
+    // Through the rounding, the last and first coordinate have a lower chance to be selected
+    // since they only have one half of the rounding.
+    let number_for_coordinate = (free_coordinate_count as f32 * random_number).round();
+    let index_for_cooridnate = (number_for_coordinate - 1.0) as usize;
+    index_for_cooridnate
 }
 
 fn get_coordinates_of_empty_cells(field: Field) -> Vec<Coordinate> {
@@ -29,12 +43,13 @@ fn get_coordinates_of_empty_cells(field: Field) -> Vec<Coordinate> {
     vec
 }
 
-fn get_new_number(random_number_value: RandomNumber) -> BoardValue {
+fn get_new_board_value(random_number: RandomNumber) -> BoardValue {
     const TWO: BoardValue = BoardValue::new(2);
     const FOUR: BoardValue = BoardValue::new(4);
 
+    // Asumption: the four appears roughly in 10% of the cases
     let new_number: BoardValue;
-    if random_number_value < 0.9 {
+    if random_number < 0.9 {
         new_number = TWO;
     } else {
         new_number = FOUR;
@@ -64,12 +79,12 @@ mod tests {
         [TWO, TWO, TWO, TWO],
         [TWO, TWO, TWO, TWO],
     ];
+    const FAKE_RANDOM_NUMBER_VALUE_ZERO: RandomNumber = 0.0;
     const FAKE_RANDOM_NUMBER_POSITION_ZERO: RandomNumber = 0.0;
 
     #[cfg(test)]
     mod given_random_number_value_zero {
         use crate::field_add_random_value::tests::*;
-        const FAKE_RANDOM_NUMBER_VALUE_ZERO: RandomNumber = 0.0;
         #[test]
         fn it_should_return_field_with_two_in_the_first_empty_cell_for_empty_field() {
             let new_field = add_value(
@@ -158,6 +173,69 @@ mod tests {
             );
             let expected_field = [[FOUR, X, X, X], [X, X, X, X], [X, X, X, X], [X, X, X, X]];
             assert_eq!(new_field, expected_field);
+        }
+    }
+
+    #[cfg(test)]
+    mod given_random_number_position_zero_point_nine_nine {
+        use crate::field_add_random_value::tests::*;
+        const FAKE_RANDOM_NUMBER_POSITION_ZERO_POINT_NINE_NINE: RandomNumber = 0.99;
+        #[test]
+        fn it_should_return_field_with_two_in_the_last_empty_cell_for_empty_field() {
+            let new_field = add_value(
+                EMPTY_FIELD,
+                FAKE_RANDOM_NUMBER_VALUE_ZERO,
+                FAKE_RANDOM_NUMBER_POSITION_ZERO_POINT_NINE_NINE,
+            );
+            let expected_field = [[X, X, X, X], [X, X, X, X], [X, X, X, X], [X, X, X, TWO]];
+            assert_eq!(new_field, expected_field);
+        }
+        #[test]
+        fn it_should_return_field_with_two_in_the_last_empty_cell_for_non_empty_field() {
+            let new_field = add_value(
+                NON_EMPTY_FIELD,
+                FAKE_RANDOM_NUMBER_VALUE_ZERO,
+                FAKE_RANDOM_NUMBER_POSITION_ZERO_POINT_NINE_NINE,
+            );
+            let expected_field = [[TWO, X, X, X], [X, X, X, X], [X, X, X, X], [X, X, X, TWO]];
+            assert_eq!(new_field, expected_field);
+        }
+        #[test]
+        fn it_should_return_field_with_two_in_the_last_empty_cell_for_last_position_filled_field() {
+            const LAST_POSITION_FILLED_FIELD: Field =
+                [[X, X, X, X], [X, X, X, X], [X, X, X, X], [X, X, X, TWO]];
+            let new_field = add_value(
+                LAST_POSITION_FILLED_FIELD,
+                FAKE_RANDOM_NUMBER_VALUE_ZERO,
+                FAKE_RANDOM_NUMBER_POSITION_ZERO_POINT_NINE_NINE,
+            );
+            let expected_field = [[X, X, X, X], [X, X, X, X], [X, X, X, X], [X, X, TWO, TWO]];
+            assert_eq!(new_field, expected_field);
+        }
+    }
+
+    #[cfg(test)]
+    mod given_random_number_position_zero_point_five_four {
+        use crate::field_add_random_value::tests::*;
+        const FAKE_RANDOM_NUMBER_POSITION_ZERO_POINT_FIVE: RandomNumber = 0.54;
+        #[test]
+        fn it_should_return_field_with_two_in_the_nineth_empty_cell_for_empty_field() {
+            let new_field = add_value(
+                EMPTY_FIELD,
+                FAKE_RANDOM_NUMBER_VALUE_ZERO,
+                FAKE_RANDOM_NUMBER_POSITION_ZERO_POINT_FIVE,
+            );
+            let expected_field = [[X, X, X, X], [X, X, X, X], [TWO, X, X, X], [X, X, X, X]];
+            assert_eq!(new_field, expected_field);
+        }
+        #[test]
+        fn it_should_return_field_completly_filled_for_only_one_empty_cell_field() {
+            let new_field = add_value(
+                ONLY_ONE_EMPTY_CELL_FIELD,
+                FAKE_RANDOM_NUMBER_VALUE_ZERO,
+                FAKE_RANDOM_NUMBER_POSITION_ZERO_POINT_FIVE,
+            );
+            assert_eq!(new_field, FULL_FIELD);
         }
     }
 }
